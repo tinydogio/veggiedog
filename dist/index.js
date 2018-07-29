22cda3927bc56865228ecb61f8ff4e808510aa47
Joshua Hamilton <joshua@tinydog.io>
2018-07-29 19:57:54 -0400

    Add build files and config change.

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// TODO: Remove. This is a test to see how publishing works.
+function add(a, b) {
+    return a + b;
+}
+exports.add = add;
+//# sourceMappingURL=index.js.map
